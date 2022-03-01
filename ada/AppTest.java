27a539a82cae769330336af8204a349475653cbd
EmaRCB <a17003636@alumnos.uady.mx>
2022-02-28 18:20:23 -0600

    java main

--- ada/AppTest.java
+++ ada/AppTest.java
@@ -7,41 +7,20 @@ import org.antlr.v4.runtime.tree.*;
 
 public class AppTest {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ingresa tu cadena: ");
         Scanner sc = new Scanner(System.in);
+        System.out.println("A continuación, ingresa la cadena a evaluar. En caso de querer abandonar el proceso, escriba /o");
+        System.out.println("Ingresa tu cadena: ");
         String iString = sc.nextLine();
-        while (!iString.equals("(exit)")) {
-            lispLexerParserEvaluator(iString);
-            System.out.print("(tlisp) > ");
-            iString = sc.nextLine();
+        while (!iString.equals("/o")) {
+            CharStream input = CharStreams.fromString(iString);
+            OperacionesLexer lexer = new OperacionesLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            OperacionesParser parser = new OperacionesParser(tokens);
+            ParseTree tree = parser.exprS();
+            tree = parser.exprS();
+
+            System.out.println("Arbol de expresión: " + tree.toStringTree(parser));
+            System.out.println("Resultado: " + (new TLispPrefixEvaluator()).evaluate(iString));
         }
         sc.close();
     }
-
-    public static void lispLexerParserEvaluator(String iString) {
-        // create a CharStream that reads from a String
-        CharStream input = CharStreams.fromString(iString);
-        // create a lexer that feeds off of input CharStream
-        OperacionesLexer lexer = new OperacionesLexer(input);
-        // create a buffer of tokens pulled from the lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        // create a parser that feeds off the tokens buffer
-        OperacionesParser parser = new OperacionesParser(tokens);
-        ParseTree tree = parser.exprS(); // begin parsing at exprS rule
-
-        try {
-            tree = parser.exprS(); // begin parsing at exprS rule+
-
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println("That' not a valid expression.");
-            return;
-        }
-
-        // System.out.println("Expression tree: " + tree.toStringTree(parser)); // print
-        // LISP-style tree
-        System.out.println("Expression tree: " + tree.toStringTree(parser));
-        System.out.println("Val: " + (new TLispPrefixEvaluator()).evaluate(iString));
-
-    }
-}
